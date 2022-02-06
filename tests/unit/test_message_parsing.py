2846fe1a0190ee69b32d9e6d248b7c182022c612
Odumosu Dipo <ironkyo@gmail.com>
2022-02-06 21:12:33 +0100

    chore: format using black

--- tests/unit/test_message_parsing.py
+++ tests/unit/test_message_parsing.py
@@ -6,20 +6,20 @@ from app.utils import parse_message
 
 
 def test_message_parsing():
-    sample_text1 = f'''amount: {CURRENCY} 5 description: stash'''
+    sample_text1 = f"""amount: {CURRENCY} 5 description: stash"""
     result = parse_message(sample_text1)
 
     assert result is not None
-    assert result.get('amount') == decimal.Decimal(5)
-    assert result.get('description') == 'stash'
+    assert result.get("amount") == decimal.Decimal(5)
+    assert result.get("description") == "stash"
 
-    sample_text2 = f'''
+    sample_text2 = f"""
     description: rounding error
     amount: {CURRENCY} .5
 
 
-    '''
+    """
     result = parse_message(sample_text2)
     assert result is not None
-    assert result.get('amount') == decimal.Decimal(0.5)
-    assert result.get('description') == 'rounding error'
+    assert result.get("amount") == decimal.Decimal(0.5)
+    assert result.get("description") == "rounding error"
